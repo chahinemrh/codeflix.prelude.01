92bfc12cef06aeff85706517968d636e5432e5aa
chahinemrh <chahinemrh@gmail.com>
2019-10-23 23:04:06 +0200

    r

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 //capitalize = require( './exo4');
 //decapitalize = require('./exo5');
 //kebabCase = require('./exo6');
-first= require('./exo10');
-last= require('./exo11');
-count= require('./exo14');
-substr= require('./exo12');
+//first= require('./exo10');
+//last= require('./exo11');
+//count= require('./exo14');
+//substr= require('./exo12');
 
+ //lowerCase = require("./ex01")
+// upperCase = require("./ex02")
+// stringLength = require("./ex03")
+
+// snakeCase = require("./ex07")
+// upperTolower = require("./ex08")
+// charAt = require("./ex09")
+
+// slice = require("./ex13")
+// countSubstrings = require("./ex16")
+// indexOf = require("./ex17")
+// lastIndexOf = require("./ex18")
+// insert = require("./ex19")
 
 console.log(substr('chahine',2,4));
